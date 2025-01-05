926b4b8811194ae07a068d455f17726f6beb7079
RiteshRG <riteshgawde5@gmail.com>
2025-01-06 00:39:51 +0530

    1st commit (Auth UI)

---
+++ android/app/src/main/kotlin/com/example/donornet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.donornet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/materials/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Define primary and accent colors
+  static const Color primaryColor = Color.fromARGB(255, 38, 182, 122); 
+  static const Color secondaryColor = Color.fromARGB(255, 35, 151, 103);
+  static const Color tertiaryColor = Color.fromARGB(255, 15, 119, 125);
+  }
